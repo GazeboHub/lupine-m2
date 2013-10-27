9bf2bce92bb4802d5e4badb3e700a8c640e402c8
Sean Champ <spchamp+git-muletop@me.com>
2013-10-27 09:47:44 -0700

    add NAMESAPCE-PACKAGE; ENSURE-P -> MOVE-P in BIND-PREFIX; cleanups

--- module/xmi/src/main/lisp/util-cxml.lisp
+++ module/xmi/src/main/lisp/util-cxml.lisp
@@ -47,7 +47,10 @@
 (defstruct (namespace
 	    (:include finalizable-instance)
 	    (:conc-name #:namespace-)
-	    (:constructor %make-namespace (string uri)))
+	    (:constructor %make-namespace (string uri
+					   &optional
+					     (package
+					      (find-package* string)))))
   (string #.(simplify-string "")
    :type simple-string)
   (uri (allocate-instance (find-class 'puri:uri))
@@ -60,6 +63,10 @@
 			    :fill-pointer 0)
    :type (or (vector simple-string) simple-vector))
 
+  (package (find-package "" t)
+   :type package
+   :read-only t)
+
   (local-names-table (make-hash-table
 		      :test #'equal
 		      :size #.+qname-buffer-extent+)
@@ -67,13 +74,23 @@
    ;; NAMESPACE-PACKAGE for NAMESPACE (using EQ for the hash table)
    :type hash-table))
 
+#|
+ (let ((ns (simplify-string "CL")))
+  (defparameter *ns*
+    (%make-namespace ns (puri:intern-uri ns))))
+
+ (eq (namespace-package *ns*) (find-package "COMMON-LISP"))
+ =EXPECT=> T
+|#
+
+
 (defmethod print-object ((instance namespace) stream)
   (print-unreadable-object (instance stream :type t :identity t)
-    (princ (namespace-prefix-table instance) stream)
-    (write-char #\Space stream)
-    (princ (namespace-string instance) stream)
-    (write-char #\Space stream)
-    (princ (instance-finalized-p instance) stream)))
+    (format stream "~A ~A ~A ~A"
+	    (namespace-prefix-table instance)
+	    (namespace-string instance)
+	    (instance-finalized-p instance)
+	    (package-shortest-name (namespace-package instance)))))
 
 (defmethod finalize ((instance namespace))
   ;; FIXME: call FINALIZE sometime after model initialization
@@ -104,9 +121,8 @@
 (defun make-namespace (ns-string)
   (declare (type string ns-string))
   (let ((s (simplify-string ns-string)))
-    (%make-namespace
-     ns-string
-     (puri:intern-uri ns-string *qname-ns-registry*))))
+    (%make-namespace s
+		     (puri:intern-uri s *qname-ns-registry*))))
 
 (defun ensure-qname-string (ncname ns )
   (declare (type string ncname)
@@ -134,6 +150,7 @@
 |#
 
 (defun find-prefix (prefix namespace)
+  ;; FIXME: ADD ERRORP
   (declare (type simple-string prefix)
 	   (type namespace namespace)
 	   (values (or null simple-string) &optional))
@@ -161,10 +178,9 @@
 
 (defmethod print-object ((instance namespace-registry) stream)
   (print-unreadable-object (instance stream :type t :identity t)
-    (princ (length (namespace-registry-namespace-table instance))
-	   stream)
-    (write-char #\Space stream)
-    (princ (instance-finalized-p instance) stream)))
+    (format stream "~D ~A"
+	    (length (namespace-registry-namespace-table instance))
+	    (instance-finalized-p instance))))
 
 
 (defmethod finalize ((instance namespace-registry))
@@ -190,16 +206,11 @@
 		      :fill-pointer len
 		      :adjustable t
 		      :initial-contents table))
-    (do-vector (ns (the simple-vector table) instance)
+    (do-vector (ns table instance)
       (unfinalize ns))))
 
 
-;;; * Condition Types NAME-CONDITION, NAMESPACE-CONDITION
-
-(define-condition name-condition ()
-  ((name
-    :initarg :name
-    :reader name-condition-name)))
+;;; * Condition Type NAMESPACE-CONDITION
 
 
 (define-condition namespace-condition ()
@@ -257,8 +268,8 @@
 
   "Ensure that a namespace object exists for the string URI within the
 specified NAMESPACE-REGISTRY. Returns the namespace object and a
-second, boolean value indicating whether the namespace object was
-created newly in this evaluation."
+boolean value indicating whether the namespace object was created
+newly in this evaluation."
   (declare (type string namespace)
 	   (type namespace-registry registry)
 	   (values namespace boolean &optional))
@@ -270,20 +281,20 @@ created newly in this evaluation."
 	     (do-vector (ns table)
 	       (declare (type namespace ns))
 	       (when (string= the-string (namespace-string ns))
-		 (return (values ns nil))))))
-      (or (find-registered)
-	  (cond
-	    (ensure-p
-	     (assert-not-finalized registry
-				   "Unable to add instance for namespace ~s"
-				   namespace)
-	     (let ((reg (make-namespace the-string)))
-	       (vector-push-extend reg table)
-	       (values reg t)))
-	    (t (error 'namespace-not-found-error
-		      :name namespace
-		      :namespace registry)))
-	  ))))
+		 (return ns)))))
+      (let ((ns (find-registered)))
+	(cond
+	  (ns (values ns nil))
+	  (ensure-p
+	   (assert-not-finalized registry
+				 "Unable to add instance for namespace ~s"
+				 namespace)
+	   (let ((ns-new (make-namespace the-string)))
+	     (vector-push-extend ns-new table)
+	     (values ns-new t)))
+	  (t (error 'namespace-not-found-error
+		    :name namespace
+		    :namespace registry)))))))
 
 ;;; * Condition Type Protocol: Namespace Binding/Unbinding
 
@@ -292,13 +303,9 @@ created newly in this evaluation."
 
 (defmethod print-object ((instance namespace-prefix-condition) stream)
   (print-unreadable-object (instance stream :type t :identity t)
-    (write-string (name-condition-name instance) stream)
-    (write-char #\Space stream)
-    (let ((ns (namespace-condition-namespace instance)))
-      (typecase ns
-	;; fixme: frobbed dispatching
-	(namespace (write-string (namespace-string ns) stream))
-	(t (princ ns stream))))))
+    (format stream "~A ~A"
+	    (name-condition-name instance)
+	    (namespace-condition-namespace instance))))
 
 (define-condition namespace-prefix-bind (namespace-prefix-condition)
   ()
@@ -325,9 +332,9 @@ created newly in this evaluation."
   ()
   (:report
    (lambda (c s)
-     (fomat s "Prefix ~s is already bound to ~s"
-	    (name-condition-name c)
-	    (namespace-condition-namespace c)))))
+     (format s "Prefix ~s is already bound to ~s"
+	     (name-condition-name c)
+	     (namespace-condition-namespace c)))))
 
 (define-condition prefix-already-bound-error (error
 					      prefix-already-bound)
@@ -362,12 +369,11 @@ that namesapce is returned as the second value.
 When PREFIX is not found for any namespace within REGISTRY and ERRORP
 is \"True\", an error of type PREFIX-NOT-FOUND-ERROR is signaled"
   (declare (type string prefix)
-	   (type namespace-registry regisry)
+	   (type namespace-registry registry)
 	   (values (or namespace null)
-		   (or string null)
+		   (or simple-string null)
 		   &optional))
-  (let ((namespaces (namespace-registry-namespace-table registry))
-	ns-p pf-p)
+  (let ((namespaces (namespace-registry-namespace-table registry)))
     (do-vector (ns namespaces (cond
 				(errorp
 				 (error 'prefix-not-found-error
@@ -380,7 +386,7 @@ is \"True\", an error of type PREFIX-NOT-FOUND-ERROR is signaled"
 	  (return (values ns p)))))))
 
 
-(defun bind-prefix (prefix uri registry &optional ensure-p)
+(defun bind-prefix (prefix uri registry &optional move-p)
 "Ensure that the namespace prefix PREFIX is registered uniquely to
 the namespace URI in namespace-registry REGISTRY.
 
@@ -399,13 +405,9 @@ NAMESPACE-PREFIX-BIND is signaled.
 
 ## Exceptional Situations
 
-An error of type PREFIX-ALREADY-BOUND-ERROR is signaled when ENSURE-P
+An error of type PREFIX-ALREADY-BOUND-ERROR is signaled when MOVE-P
 is \"False\" and PREFIX denotes a prefix bound to a namespace not
 namespace-equivalent (that is, string-equivalent) to URI
-
-An error of type PREFIX-NOT-FOUND-ERROR is signaled when ENSURE-P
-is \"False\" and PREFIX denotes a prefix not present in the namespace
-for URI within REGISTRY
 
 An error of type SIMPLE-INSTANCE-FINALIZED-ERROR is signaled when a
 new binding must be created in either the REGISTRY or the namespace,
@@ -429,23 +431,31 @@ though the respective object would be FINALIZED-P"
 	    ((and ns-p (string= (namespace-string ns-p) urix))
 	     (return-from local-binding
 	       (values ns-p pf-p)))
-	    ((and ns-p ensure-p)
+	    ((and ns-p move-p)
 	     (assert-not-finalized ns-p
 				   "Unable to add prefix ~s"
 				   pf-p)
 
 	     ;; FIXME: Thread safety onto NS-P - lock NS-P
-	     (delete pf-p (namespace-prefix-table ns-p)
-		     :test #'eq)
+	     (setf (namespace-prefix-table ns-p)
+		   (delete pf-p (namespace-prefix-table ns-p)
+			   :test #'eq))
+
+	     ;; FIXME: when the unbind signal is caught in
+	     ;; a containing handler-case calling this function
+	     ;; within its body, then even after 'continue' in that
+	     ;; handler-case, the later binding is not made
 	     (restart-case
 		 (signal 'namespace-prefix-unbind
 			 :name pf-p
 			 :namespace ns-p)
-	       (continue)))
+	       (continue ()
+		 #+NIL ;; not the right approach
+		 (values ns-p pf-p)))
 	    (ns-p
 	     (error 'prefix-already-bound-error
 		    :name pf-p
-		    :namespaace ns-p))
+		    :namespace ns-p))
 	    ))) ;; block ns-prefix
       (block ns-uri
 	;; 1) check for URI in Registry
@@ -456,7 +466,7 @@ though the respective object would be FINALIZED-P"
 	;; 1.B.B) if not, add prefix to namespace, emit signal
 	;;        PREFIX-BIND
 	(multiple-value-bind (ns ns-new-p)
-	    (compute-namespace uri registry ensure-p)
+	    (compute-namespace uri registry move-p)
 	  (flet ((add-prefix ()
 		   (assert-not-finalized ns
 					 "Unable to add prefix ~s"
@@ -468,7 +478,8 @@ though the respective object would be FINALIZED-P"
 		       (signal 'namespace-prefix-bind
 			       :namespace ns
 			       :name pfx)
-			(continue))
+		     (continue))
+
 		   (values ns pfx)))
 	    (cond
 	      (ns-new-p (add-prefix))
@@ -479,14 +490,10 @@ though the respective object would be FINALIZED-P"
 		   (pf-p
 		    (return-from local-binding
 		      (values ns pf-p)))
-		   (ensure-p (add-prefix))
-		   (t
-		    (error 'prefix-not-found-error
-			   :name prefix
-			   :namespace ns
-			   ))))))))) ;; block ns-uri
+		   (t (add-prefix))
+		   ))))))) ;; block ns-uri
       )  ;; block local-binding
-    ))) ;; defun
+    )) ;; defun
 
 ;; FIXME: For XML namespace support, defun ADD-DEFAULT-XML-NAMESPACES
 ;; cf. "xml" and "xmlns" prefixes' standard namespace bindings
@@ -499,32 +506,39 @@ though the respective object would be FINALIZED-P"
 
  (defparameter  *foo.ex* (simplify-string "http://foo.example.com/"))
 
- (eq (compute-namespace *foo.ex* *r*)
-     (compute-namespace *foo.ex* *r*))
+ (defpackage #.*foo.ex* (:use) (:nicknames "NS/FOO"))
+
+ (eq (compute-namespace *foo.ex* *r* t)
+     (compute-namespace *foo.ex* *r* nil))
  ;; => T
 
 
  (multiple-value-bind (ns pfx)
-     (bind-prefix *foo* *foo.ex* *r*)
+     (bind-prefix *foo* *foo.ex* *r* t)
    (values (eq pfx *foo*) ns )
    )
   ;; => T, #<structure-object ...>
 
 ;; (resolve-prefix-namespace "foo" *r*) ;; OK
-;; (resolve-prefix-namespace "wtf" *r*) ;; expect error
-;; (resolve-prefix-namespace "wtf" *r* nil) ;; expect NIL, NIL
+;; (resolve-prefix-namespace "nope" *r*) ;; expect error
+;; (resolve-prefix-namespace "2xNull" *r* nil) ;; expect NIL, NIL
 
 ;; (aref (namespace-registry-namespace-table *r*) 0)
+;; ^ informative
 
 
- (bind-prefix "bar" *foo.ex* *r*)
+
+ (bind-prefix "bar" *foo.ex* *r* t)
  ;; ^ call multiple times, should not be duplicating prefixes
 
+ (defpackage "http://bar.example.com/")
+
  (handler-case
-     (bind-prefix "bar" "http://bar.example.com/" *r*)
+     (bind-prefix "bar" "http://bar.example.com/" *r* t)
    (namespace-prefix-unbind (c)
      (warn "Caught unbind signal: ~s" c)
-     (continue c)))
+     ;; ^ expect warning message only on first call
+   (continue c)))
  ;; expect, always - noting that the signal should happen only once
  ;;   (length (namespace-registry-namespace-table *r*))
  ;;    => 2
@@ -547,9 +561,9 @@ though the respective object would be FINALIZED-P"
       (split-string-1 #\: cname)
 
     (unless (cxml::valid-ncname-p prefix)
-      (type-error :expected-type 'ncname :datum prefix))
+      (error 'type-error :expected-type 'ncname :datum prefix))
     (unless (cxml::valid-ncname-p name)
-      (type-error :expected-type 'ncname :datum name))
+      (error 'type-error :expected-type 'ncname :datum name))
 
     (let ((ns (resolve-prefix-namespace prefix registry)))
       (unless ns
@@ -558,3 +572,12 @@ though the respective object would be FINALIZED-P"
 	       :namespace registry))
       (values (ensure-qname-string name ns)
 	      ns))))
+
+(defun ensure-qname-symbol (cname registry)
+    (declare (type string cname)
+	     (type namespace-registry registry)
+	     (values symbol namespace &optional))
+  (multiple-value-bind (ncname ns)
+      (ensure-qname cname registry)
+    (values (intern ncname (namespace-package ns))
+	    ns)))
