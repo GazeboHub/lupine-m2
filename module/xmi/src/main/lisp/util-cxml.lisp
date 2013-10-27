f69df45c9e2f50ef7ee9295e490a9f5d535a34a2
Sean Champ <spchamp+git-muletop@me.com>
2013-10-27 15:56:41 -0700

    add initial compute-source-uri methods

--- module/xmi/src/main/lisp/util-cxml.lisp
+++ module/xmi/src/main/lisp/util-cxml.lisp
@@ -15,3 +15,41 @@
 
 (deftype parser-input-source ()
   '(or pathname stream string runes:xstream))
+
+;;; * Source->URI Computation
+
+
+(defgeneric compute-source-uri (source)
+  (:method ((source pathname))
+    (let ((pbuff (make-string-output-stream))
+	  (%source (merge-pathnames source)))
+      (write-char #\/ pbuff)
+      (dolist (p (cdr (pathname-directory %source)))
+	(write-string p pbuff)
+	(write-char #\/ pbuff))
+      (let* ((n (pathname-name %source))
+	     (tp (pathname-type %source)))
+	(when n
+	  (write-string n pbuff)
+	  (when tp
+	    (write-char #\. pbuff)
+	    (write-string tp pbuff))))
+      (make-instance 'puri:uri
+		     :scheme :file
+		     :host nil
+		     :path  (get-output-stream-string pbuff))))
+  (:method ((source file-stream))
+    (compute-source-uri (pathname source)))
+  #+TO-DO
+  (:method ((source a-socket-stream-in-a-network-transport-app))
+    )
+  #+TO-DO
+  (:method ((source runes:xstream))
+    ))
+
+#|
+(compute-source-uri
+ (make-pathname :directory '(:absolute "tmp" "foo")
+		:name "foo"
+		:type "md"))
+|#
