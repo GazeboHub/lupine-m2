2fd0a9d9ab0908c3920c8c04b97395c8843158ae
Sean Champ <spchamp+git-muletop@me.com>
2013-10-29 13:55:48 -0700

    correct for correctly capturing KLACKS:CONSUME return values

--- module/xmi/src/main/lisp/transform-klacks.lisp
+++ module/xmi/src/main/lisp/transform-klacks.lisp
@@ -89,7 +89,7 @@ for ~s found in metamodel-namespace-registry ~s of ~s and not ~
 	   (values model boolean &optional))
   ;; cf. `cxml-rng:parse-schema' for klacks usage reference
   ;; also <http://common-lisp.net/project/cxml/klacks.html#sources>
-  (let (model-element
+  (let (model-element element-container
 	model
 	xml-version
 	xml-document-encoding
@@ -98,78 +98,85 @@ for ~s found in metamodel-namespace-registry ~s of ~s and not ~
       (handler-case
 	  (block klacks-parse
 	    (loop
-	      (multiple-value-bind (event-type &rest event-data)
-		  (klacks:consume s)
-		(macrolet ((warn-unsupported ()
-			     (warn 'unsupported-event-type
-				   :event event-type
-				   :event-data event-data)))
-		  (ecase event-type
-		    ((nil)
-		     ;; return model as created so far.
-		     ;; second value indicates "did not complete document"
-		     ;; note that this does not call INITIALIZE-MODEL
-		     (return-from klacks-parse (values model nil)))
-		    (:start-document
-		     ;; version, encoding, standalonep
-		     (multiple-value-bind (v enc s)
-			 ;; FIXME: Parse string values for numeric biding
-			 (setq xml-version v
-			       xml-document-encoding
-			       (intern enc '#:xml/encoding)
-			       xml-standalone-p s)))
-		    (:dtd ;; name, public-id, system-id
-		     (warn-unsupported))
-		    (:start-element ;; uri, lname, qname
+	(destructuring-bind  (event-type &rest event-data)
+	    ;; NB: The "Variable number of return values" behavior
+	    ;; used in KLACKS:CONSUME and similar functions may seem
+	    ;; to essentially require some consing, to capture the
+	    ;; variable number of return values after the first return
+	    ;; value, for every Klacks event type
+	    (multiple-value-list (klacks:consume s))
+	  (macrolet ((warn-unsupported ()
+		       (warn 'unsupported-event-type
+			     :event event-type
+			     :event-data event-data)))
+	    (ecase event-type
+	      ((nil)
+	       ;; return model as created so far.
+	       ;; second value indicates "did not complete document"
+	       ;; note that this does not call INITIALIZE-MODEL
+	       (return-from klacks-parse (values model nil)))
+	      (:start-document
+	       ;; version, encoding, standalonep
+	       (destructuring-bind (v enc s)
+		   ;; FIXME: Parse string values for numeric biding
+		   (setq xml-version v
+			 xml-document-encoding
+			 (intern enc '#:xml/encoding)
+			 xml-standalone-p s)))
+	      (:dtd ;; name, public-id, system-id
+	       (warn-unsupported))
+	      (:start-element ;; uri, lname, qname
 		  	;;; 1) Detect root XMI element, intialize MODEL
 		  	;;; 2) Else, ALLOCATE-ELEMENT and map ATTRS "here"
-		     (multiple-value-bind (uri lname qname) event-data
-		       (declare (ignore qname) (type simple-string uri lname))
-		       (let ((qname-symbol (intern-qname-symbol
-					    uri lname serialization-model)))
-			 (cond
-			   ((eq qname-symbol (quote ns/xmi:XMI))
-			    (setq model (allocate-model
-					   xml-document-encoding
-					   source serialization-model)))
-			   (t
-			    (setq model-element
-				  (allocate-element
-				   qname-symbol
-				   uri lname model source
-				   serialization-model)))))
-		       (klacks:map-attributes
-			(lambda (uri lname qname val defaulted-p)
-			  (declare (simple-string uri lname val)
-				   (ignore qname defaulted-p))
-			  (let ((qs (intern-qname-symbol
-				     uri lname serializatino-model)))
-			    (apply-atrribute qs uri lname
-					     model-element model
-					     source
-					     serialization-model)))
-			source)))
-		    (:end-element ;; uri, lname, qname
-		     ;; "close" model-element
-		     (final-initialize model-element
-				       model
-				       serialization-model))
-		    (:characters ;; data
-		     ;; FIXME: Normalize string - optional or
-		     ;; element-dependent - handle in ADD-CDATA
-		     ;; methods
-		     (add-cdata xml-document-encoding data
-				model-element model))
-		    (:processing-instruction ;; pi-target, data
-		     (warn-unsupported))
-		    (:comment ;; data
-		     (warn-unsupported))
-		    (:end-document
-		     ;; 1. "close" and shared-initialize model
-		     (final-initialize model source serialization-model)
-		     ;; 2. then return - second value indicates
-		     ;; "Completed document"
-		     (return-from klacks-parse (values m t)))
-		    )))) ;; block klacks-parse
+	       (destructuring-bind (uri lname qname) event-data
+		 (declare (ignore qname) (type simple-string uri lname))
+		 (let ((qname-symbol (intern-qname-symbol
+				      uri lname serialization-model)))
+		   (cond
+		     ((eq qname-symbol (quote ns/xmi:XMI))
+		      (setq model (allocate-model
+				   xml-document-encoding
+				   source serialization-model)))
+		     (t
+		      (setq
+		       element-container model-element
+		       model-element  (allocate-element
+				       qname-symbol
+				       uri lname model source
+				       serialization-model)))))
+		 (klacks:map-attributes
+		  (lambda (uri lname qname val defaulted-p)
+		    (declare (simple-string uri lname val)
+			     (ignore qname defaulted-p))
+		    (let ((qs (intern-qname-symbol
+			       uri lname serializatino-model)))
+		      (apply-atrribute value qs uri lname
+				       model-element model
+				       source
+				       serialization-model)))
+		  source)))
+	      (:end-element ;; uri, lname, qname
+	       ;; "close" model-element
+	       (final-initialize model-element
+				 element-container
+				 model
+				 serialization-model))
+	      (:characters ;; data
+	       ;; FIXME: Normalize string - optional or
+	       ;; element-dependent - handle in ADD-CDATA
+	       ;; methods
+	       (add-cdata xml-document-encoding data
+			  model-element model))
+	      (:processing-instruction ;; pi-target, data
+	       (warn-unsupported))
+	      (:comment ;; data
+	       (warn-unsupported))
+	      (:end-document
+	       ;; 1. "close" and shared-initialize model
+	       (final-initialize model source serialization-model)
+	       ;; 2. then return - second value indicates
+	       ;; "Completed document"
+	       (return-from klacks-parse (values m t)))
+	      )))) ;; block klacks-parse
 	    ;; handle errors that the klacks parser might produce...
 	    )))))
