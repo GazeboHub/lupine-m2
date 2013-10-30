0724d3f54ded4cea57d8e3a0ced9a7251c59d157
Sean Champ <spchamp+git-muletop@me.com>
2013-10-30 10:42:41 -0700

    use class-precedence-list in compute-direct-slot-definitions

--- module/mop/src/main/lisp/aux.lisp
+++ module/mop/src/main/lisp/aux.lisp
@@ -3,11 +3,6 @@
 
 
 (defun compute-direct-slot-definitions (name class)
-  ;; By preferring class-direct-superclasses, this does not rely on
-  ;; the CLOS framework's ordered sort of the initial class' class
-  ;; precedence list.
-  ;;
-  ;; FIXME: iterate over the class precedence list, instead.
   (declare (type symbol name)
 	   (type class-designator class)
 	   (values list &optional))
@@ -17,14 +12,15 @@
     (flet ((compute-on-dsupers ()
 	     (mapcan #'(lambda (c)
 			 (compute-direct-slot-definitions name c))
-		     (class-direct-superclasses c))))
-    (cond
+		     (cdr (class-precedence-list  c)))))
+      (cond
       (it (cons it (compute-on-dsupers)))
       (t (compute-on-dsupers))))))
 
 ;; (defclass a () ((a-1)))
 ;; (defclass b (a) ((b-1) (a-1)))
 ;; (defclass c (b) ((a-1) (b-1) (c-1)))
+;; (dolist (it '(a b c)) (finalize-inheritance  (compute-class it)))
 
 ;; (compute-direct-slot-definitions 'a-1 'c)
 
